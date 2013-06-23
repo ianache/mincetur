3a5cd74698583064e2079ef13280f8feef72f159
ILVER ANACHE PUPO <ilver.anache@gmail.com>
2013-06-23 13:19:15 -0500

    Registro Inicial del Repositorio

---
+++ diseño/basedatos/data_inicial.sql
@@ -0,0 +1,6 @@
+-- TIPOS DE PLAZOS
+insert into E_TIPO_PLAZO (id,nombre) values(0, 'PLAZO.PROCEDIMIENTO');
+insert into E_TIPO_PLAZO (id,nombre) values(1, 'PLAZO.PRESENTAR.APELACION');
+insert into E_TIPO_PLAZO (id,nombre) values(2, 'PLAZO.VENCIMIENTO.APELACION');
+insert into E_TIPO_PLAZO (id,nombre) values(3, 'PLAZO.PRESENTAR.RECONSIDERACION');
+insert into E_TIPO_PLAZO (id,nombre) values(4, 'PLAZO.VENCIMIENTO.RECONSIDERACION');
---
+++ diseño/basedatos/register_jdbc_lib.cmd
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=c:\oraclexe\app\oracle\product\11.2.0\server\jdbc\lib\ojdbc6.jar -DgroupId=oracle -DartifactId=jdbc -Dversion=11.2.0 -Dpackaging=jar -DlocalRepositoryPath=.\libs\
